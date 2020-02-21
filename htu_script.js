0c4328421d37154ce0379dbbdf4ffe26f9fb8c2d
Kevin <kramirez234@west-mec.org>
2020-02-20 18:25:09 -0700

    Done for today 2-20-20
    HTU and Story

---
+++ htu_script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+//This script is going to change color based on langauge 
+
+//Changes the instructions to english
+function english(){
+
+}
+//changes the instructions to spanish
+function spanish(){
+
+}
+//Changes the intructions to french
+function french(){
+
+}
+//changes the instructions to irish
+function irish(){
+
+}
