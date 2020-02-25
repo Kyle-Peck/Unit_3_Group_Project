ac270016532e529f0a4f325bb8fd835481a5f367
Jimmy <jtorres244@west-mec.org>
2020-02-24 18:27:31 -0700

    Constructed Casual Page

--- Cat./Casual/casualNav.js
+++ Cat./Casual/casualNav.js
@@ -1,8 +1,50 @@
-
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  changeBG(n);
+}
+
+//swamps style sheet
+
+
+function changeBG(n){
+  if(n==1){
+    document.getElementById("pagestyle").setAttribute("href", "casualSty.css")
+  }else if(n==2){
+    document.getElementById("pagestyle").setAttribute("href", "casualSty2.css")
+  }else{
+    document.getElementById("pagestyle").setAttribute("href", "casualSty3.css")
+  }
 }
